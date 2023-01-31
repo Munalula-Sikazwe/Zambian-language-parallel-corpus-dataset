c1f4de837a46f070d07bc9594c687779e1dbdaff
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-02-01 01:12:08 +0200

    successfully creating dataframe .

---
+++ bible_books.py
@@ -0,0 +1,242 @@
+bible_books = [
+    {
+        "name": "Genesis",
+        "abv": "GEN"
+    },
+    {
+        "name": "Exodus",
+        "abv": "EXO"
+    },
+    {
+        "name": "Leviticus",
+        "abv": "Lev"
+    },
+    {
+        "name": "Numbers",
+        "abv": "Num"
+    },
+    {
+        "name": "Deuteronomy",
+        "abv": "Deut"
+    },
+    {
+        "name": "Joshua",
+        "abv": "Josh"
+    },
+    {
+        "name": "Judges",
+        "abv": "Judg"
+    },
+    {
+        "name": "Ruth",
+        "abv": "Ruth"
+    },
+    {
+        "name": "1 Samuel",
+        "abv": "1 Sam"
+    },
+    {
+        "name": "2 Samuel",
+        "abv": "2 Sam"
+    },
+    {
+        "name": "1 Kings",
+        "abv": "1 Kings"
+    },
+    {
+        "name": "2 Kings",
+        "abv": "2 Kings"
+    },
+    {
+        "name": "1 Chronicles",
+        "abv": "1 Chron"
+    },
+    {
+        "name": "2 Chronicles",
+        "abv": "2 Chron"
+    },
+    {
+        "name": "Ezra",
+        "abv": "Ezra"
+    },
+    {
+        "name": "Nehemiah",
+        "abv": "Neh"
+    },
+    {
+        "name": "Esther",
+        "abv": "Esth"
+    },
+    {
+        "name": "Job",
+        "abv": "Job"
+    },
+    {
+        "name": "Psalms",
+        "abv": "Ps"
+    },
+    {
+        "name": "Proverbs",
+        "abv": "Prov"
+    },
+    {
+        "name": "Ecclesiastes",
+        "abv": "Eccl"
+    },
+    {
+        "name": "Song of Solomon",
+        "abv": "Song"
+    },
+    {
+        "name": "Isaiah",
+        "abv": "Isa"
+    },
+    {
+        "name": "Jeremiah",
+        "abv": "Jer"
+    },
+    {
+        "name": "Lamentations",
+        "abv": "Lam"
+    },
+    {
+        "name": "Ezekiel",
+        "abv": "Ezek"
+    },
+    {
+        "name": "Daniel",
+        "abv": "Dan"
+    },
+    {
+        "name": "Hosea",
+        "abv": "Hos"
+    },
+    {
+        "name": "Joel",
+        "abv": "Joel"
+    },
+    {
+        "name": "Amos",
+        "abv": "Amos"
+    },
+    {
+        "name": "Obadiah",
+        "abv": "Obad"
+    },
+    {
+        "name": "Jonah",
+        "abv": "Jonah"
+    },
+    {
+        "name": "Micah",
+        "abv": "Micah"
+    },
+    {
+        "name": "Nahum",
+        "abv": "Nahum"
+    },
+    {
+        "name": "Habakkuk",
+        "abv": "Hab"
+    },
+    {
+        "name": "Zephaniah",
+        "abv": "Zeph"
+    },
+    {
+        "name": "Haggai",
+        "abv": "Haggai"
+    },
+    {
+        "name": "Zechariah",
+        "abv": "Zech"
+    },
+    {
+        "name": "Malachi",
+        "abv": "Mal"
+    },
+    {
+        "name": "Matthew",
+        "abv": "Matt"
+    },
+    {
+        "name": "Mark",
+        "abv": "Mark"
+    },
+    {
+        "name": "Luke",
+        "abv": "Luke"
+    },
+    {
+        "name": "John",
+        "abv": "John"
+    },
+    {
+        "name": "Acts",
+        "abv": "Acts"
+    },
+    {
+        "name": "Romans",
+        "abv": "Rom"
+    },
+    {
+        "name": "1 Corinthians",
+        "abv": "1 Cor"
+    },
+    {
+        "name": "2 Corinthians",
+        "abv": "2 Cor"
+    },
+    {
+        "name": "Galatians",
+        "abv": "Gal"
+    },
+    {
+        "name": "Ephesians",
+        "abv": "Eph"
+    },
+    {
+        "name": "Philippians",
+        "abv": "Phil"
+    },
+    {
+        "name": "Colossians",
+        "abv": "Col"
+    },
+    {
+        "name": "1 Thessalonians",
+        "abv": "1 Thess"
+    },
+    {
+        "name": "2 Thessalonians",
+        "abv": "2 Thess"
+    },
+    {
+        "name": "1 Timothy",
+        "abv": "1 Tim"
+    },
+    {
+        "name": "2 Timothy",
+        "abv": "2 Tim"
+    },
+    {"name": "Titus", "abv": "Titus"}
+    ,
+    {"name": "Philemon", "abv": "Philem"}
+
+    ,
+    {"name": "Hebrews", "abv": "Heb"}
+    ,
+    {"name": "James", "abv": "James"}
+    ,
+    {"name": "1 Peter", "abv": "1 Pet"}
+    ,
+    {"name": "2 Peter", "abv": "2 Pet"}
+    ,
+    {"name": "1 John", "abv": "1 John"},
+    {"name": "2 John", "abv": "2 John"},
+    {"name": "3 John", "abv": "3 John"},
+    {"name": "Jude",
+     "abv": "Jude"
+     },
+    {"name": "Revelation", "abv": "Rev"}
+]
